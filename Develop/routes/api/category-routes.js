d4c6bb4755ff2caa168a27051c2936b7a66886ea
DonNavinS <seneviratnanavin@gmail.com>
2021-07-16 18:18:51 -0400

    GET and POST routes for category table are working

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,20 +1,65 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const seedCategories = require("../../seeds/category-seeds")
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  // seedCategories();
+
+  Category.findAll()
+
+  .then((userData) => {
+    res.json(userData)
+  })
+
+  .catch((err) => {
+    if (err) {
+      res.status(500).json(err)
+    }
+  }
   // be sure to include its associated Products
-});
+)});
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+// seedCategories();
+
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  .then((userData) => {
+    if (!userData) {
+      res.status(404).json({message: "No category with this ID"})
+      return;
+    }
+    res.json(userData)
+
+  })
+  .catch((err)=> {
+    if (err) {
+      res.json(err)
+    }
+  }  )
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+Category.create({
+  
+  category_name: req.body.name
+})
+
+.then((userData) => {
+  res.json(userData)
+})
+
+.catch(err => {
+  console.log(err)
+})
+
 });
 
 router.put('/:id', (req, res) => {
