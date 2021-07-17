d92c72da0e7a28ac72317dc92a38d8017f217a4e
DonNavinS <seneviratnanavin@gmail.com>
2021-07-16 23:53:56 -0400

    PUT and DELETE routes are functional for category table

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
-const seedCategories = require("../../seeds/category-seeds")
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // seedCategories();
 
   Category.findAll()
 
@@ -22,7 +20,6 @@ router.get('/', (req, res) => {
 )});
 
 router.get('/:id', (req, res) => {
-// seedCategories();
 
   Category.findOne({
     where: {
@@ -48,8 +45,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
 Category.create({
-  
-  category_name: req.body.name
+  category_name: req.body.category_name
 })
 
 .then((userData) => {
@@ -63,11 +59,44 @@ Category.create({
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+Category.update(req.body, {
+  where: {
+    id: req.params.id
+  },
+})
+
+.then(userData => {
+  if (!userData[0]) {
+    res.status(404).json({message: "There is no user with this ID"})
+    return;
+  }
+  res.json(`Updated category number ${req.params.id}`)
+})
+
+.catch(err => {
+  if (err) {
+    res.json(500).json(err)
+  }
+})
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+Category.destroy({
+  where: {
+    id: req.params.id
+  }
+})
+
+.then(userData => {
+
+  res.json(`Category with ID ${req.params.id} has been deleted`)
+
+})
+
+.catch(err => {
+  res.json(err)
+})
+
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./routes');
-const seedCategories = require("./seeds/category-seeds")
+const seedCategories = require("./seeds/category-seeds");
+const seedProducts = require('./seeds/product-seeds');
+const seedTags = require("./seeds/tag-seeds");
+const seedProductTags = require("./seeds/product-tag-seeds")
 // import sequelize connection
 
 const app = express();
@@ -13,10 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
 sequelize.sync({ force: true })
 
 .then(seedCategories)
+.then(seedProducts)
+.then(seedProductTags)
+.then(seedTags)
 
 
 .then(() => {
