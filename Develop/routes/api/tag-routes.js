ed96596cb11ae0435d9bdb3ffcb5949ef686778f
DonNavinS <seneviratnanavin@gmail.com>
2021-07-18 23:45:04 -0400

    Final commit 2

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -41,15 +41,58 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  Tag.create()
-});
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  
+  .then((data) => {
+    res.json(data)
+  })
+  
+  .catch(err => {
+    console.log(err)
+  })
+  
+  });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+  
+  .then(data => {
+    if (!data[0]) {
+      res.status(404).json({message: "There is no tag with this ID"})
+      return;
+    }
+    res.json(`Updated tag name ${req.params.id}`)
+  })
+  .catch(err => {
+    if (err) {
+      res.json(500).json(err)
+    }
+  })
+  });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  
+  .then(data => {
+  
+    res.json({message:`Tag with ID ${req.params.id} has been deleted`})
+  
+  })
+  
+  .catch(err => {
+    res.json(err)
+  })
+  
+  });
 
 module.exports = router;
